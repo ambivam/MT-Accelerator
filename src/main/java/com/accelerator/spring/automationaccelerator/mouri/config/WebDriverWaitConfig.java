6f360294ab785eb433cf52a3514dfdf84c619d4c
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-21 19:27:39 +0530

    Webdriverwaitconfig added scope prototype

--- src/main/java/com/accelerator/spring/automationaccelerator/mouri/config/WebDriverWaitConfig.java
+++ src/main/java/com/accelerator/spring/automationaccelerator/mouri/config/WebDriverWaitConfig.java
@@ -5,7 +5,9 @@ import com.accelerator.spring.automationaccelerator.mouri.annotations.LazyConfig
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @LazyConfiguration
 public class WebDriverWaitConfig {
@@ -15,6 +17,7 @@ public class WebDriverWaitConfig {
     private int timeout;
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public WebDriverWait webDriverWait(WebDriver driver){
         return new WebDriverWait(driver,this.timeout);
     }
