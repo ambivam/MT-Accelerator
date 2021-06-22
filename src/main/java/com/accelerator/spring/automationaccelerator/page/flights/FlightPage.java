1a3afc55d951ce41098d8d21e403b02b4f2e8849
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-22 09:51:52 +0530

    Created class for localization flight application 85

---
+++ src/main/java/com/accelerator/spring/automationaccelerator/page/flights/FlightPage.java
@@ -0,0 +1,34 @@
+package com.accelerator.spring.automationaccelerator.page.flights;
+
+import com.accelerator.spring.automationaccelerator.mouri.annotations.Page;
+import com.accelerator.spring.automationaccelerator.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Page
+public class FlightPage extends Base {
+
+    @FindBy(css = "span.thxQSb")
+    private List<WebElement> elements;
+
+    public void goTo(final String url){
+        this.driver.get(url);
+    }
+
+    public List<String> getlabels(){
+        this.elements
+                .stream()
+                .map(WebElement::getText)
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public boolean isAt() {
+        return this.wait.until((d) -> !this.elements.isEmpty());
+    }
+
+
+}
