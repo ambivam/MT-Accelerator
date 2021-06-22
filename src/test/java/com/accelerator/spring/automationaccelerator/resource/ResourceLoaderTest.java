3e61a2cee80fa17d3e29524d88128c6c1ca263b3
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-22 07:35:10 +0530

    Dynamic resources Resources Loader 81

---
+++ src/test/java/com/accelerator/spring/automationaccelerator/resource/ResourceLoaderTest.java
@@ -0,0 +1,43 @@
+package com.accelerator.spring.automationaccelerator.resource;
+
+import com.accelerator.spring.automationaccelerator.SpringBaseTestNGTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ResourceLoaderTest extends SpringBaseTestNGTest {
+    @Value("classpath:data/testdata2.csv")
+    private Resource resource;
+
+    @Value("${screenshot.path}")
+    private Path path;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Test(dataProvider = "getData")
+    public void resourceTest(String url,String saveas) throws IOException {
+
+        FileCopyUtils.copy(
+                resourceLoader.getResource(url).getInputStream(),
+                Files.newOutputStream(path.resolve(saveas))
+        );
+    }
+
+    @DataProvider
+    public Object[][] getData() throws IOException {
+
+        return Files.readAllLines(resource.getFile().toPath())
+                .stream()
+                .map(s -> s.split(","))
+                .toArray(Object[][]::new);
+    }
+}
