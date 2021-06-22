ed9ce1060586bf1b18564b7958fed69ce46357db
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-22 18:36:57 +0530

    Testproperty source part2

--- src/test/java/com/accelerator/spring/automationaccelerator/TPSource/IdTest.java
+++ src/test/java/com/accelerator/spring/automationaccelerator/TPSource/IdTest.java
@@ -3,7 +3,7 @@ package com.accelerator.spring.automationaccelerator.TPSource;
 import com.accelerator.spring.automationaccelerator.flights.FlightTest;
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties="app.locale=id")
+@TestPropertySource(properties={"app.locale=id","browser=chrome"})
 public class IdTest extends FlightTest {
 
 }
