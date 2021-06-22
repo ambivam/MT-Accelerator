beb4f30fc86adaeddc629a0eb0f70bf1243a5e3a
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-22 18:29:12 +0530

    Test Property Source1

---
+++ src/test/java/com/accelerator/spring/automationaccelerator/TPSource/EnTest.java
@@ -0,0 +1,10 @@
+package com.accelerator.spring.automationaccelerator.TPSource;
+
+import com.accelerator.spring.automationaccelerator.flights.FlightTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties="app.locale=en")
+public class EnTest extends FlightTest {
+
+
+}
---
+++ src/test/java/com/accelerator/spring/automationaccelerator/TPSource/IdTest.java
@@ -0,0 +1,9 @@
+package com.accelerator.spring.automationaccelerator.TPSource;
+
+import com.accelerator.spring.automationaccelerator.flights.FlightTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties="app.locale=id")
+public class IdTest extends FlightTest {
+
+}
