7d92eea5326026f8d28e9e57ecc79d4dc4dc0a5e
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-23 15:43:47 +0530

    Page object creation for additional windows

---
+++ src/main/java/com/accelerator/spring/automationaccelerator/page/window/PageA.java
@@ -0,0 +1,23 @@
+package com.accelerator.spring.automationaccelerator.page.window;
+
+import com.accelerator.spring.automationaccelerator.mouri.annotations.Page;
+import com.accelerator.spring.automationaccelerator.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Page
+public class PageA extends Base {
+
+    @FindBy(id = "area")
+    private WebElement textArea;
+
+    public void addToArea(final String msg){
+        this.textArea.sendKeys(msg);
+    }
+
+    @Override
+    public boolean isAt(){
+        return this.wait.until((d) -> this.textArea.isDisplayed());
+    }
+
+}
