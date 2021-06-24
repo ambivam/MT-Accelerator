a80e3ba5e4bf47481d0b3c88ba1b2b4500165683
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-24 08:35:07 +0530

    Custom @ Window for AOP 100

---
+++ src/main/java/com/accelerator/spring/automationaccelerator/mouri/annotations/Window.java
@@ -0,0 +1,17 @@
+package com.accelerator.spring.automationaccelerator.mouri.annotations;
+
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Page
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Window {
+    String value() default "";
+}
