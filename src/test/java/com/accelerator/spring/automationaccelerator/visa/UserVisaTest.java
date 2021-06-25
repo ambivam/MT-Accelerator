a39e6980529e267a1f5aa9714fda9bcf42e3e776
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-25 14:48:23 +0530

    Loggin with Log levels in page objects and test classes 125

--- src/test/java/com/accelerator/spring/automationaccelerator/visa/UserVisaTest.java
+++ src/test/java/com/accelerator/spring/automationaccelerator/visa/UserVisaTest.java
@@ -4,6 +4,8 @@ import com.accelerator.spring.automationaccelerator.SpringBaseTestNGTest;
 import com.accelerator.spring.automationaccelerator.entity.User;
 import com.accelerator.spring.automationaccelerator.page.visa.VisaRegistrationPage;
 import com.accelerator.spring.automationaccelerator.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.ITestContext;
 import org.testng.annotations.DataProvider;
@@ -16,6 +18,9 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserVisaTest extends SpringBaseTestNGTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserVisaTest.class);
+
 
     @Autowired
     private VisaRegistrationPage registrationPage;
@@ -35,9 +40,14 @@ public class UserVisaTest extends SpringBaseTestNGTest {
         this.registrationPage.setContactDetails(u.getEmail(),u.getPhone());
         this.registrationPage.setComments(u.getComments());
         this.registrationPage.submit();
-        System.out.println(
+
+        logger.info("Request Confirmation # INFO : "+this.registrationPage.getConfirmationNumber());
+        logger.warn("Request Confirmation # WARN : "+this.registrationPage.getConfirmationNumber());
+
+
+        /*System.out.println(
                 this.registrationPage.getConfirmationNumber()
-        );
+        );*/
     }
 
     @DataProvider
