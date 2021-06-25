ee76157e7006b53f1615a9ce1f91e4167ebf612f
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-25 12:40:51 +0530

    Parameterizing data Provider approach 1

--- src/test/java/com/accelerator/spring/automationaccelerator/UserVisaTest.java
+++ src/test/java/com/accelerator/spring/automationaccelerator/UserVisaTest.java
@@ -5,11 +5,13 @@ import com.accelerator.spring.automationaccelerator.entity.User;
 import com.accelerator.spring.automationaccelerator.page.visa.VisaRegistrationPage;
 import com.accelerator.spring.automationaccelerator.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.sql.Date;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserVisaTest extends SpringBaseTestNGTest {
@@ -20,28 +22,29 @@ public class UserVisaTest extends SpringBaseTestNGTest {
     @Autowired
     private UserRepository repository;
 
-    @Test
-    public void VisaTest(){
-        List<User> users =  this.repository.findByDobBetween(Date.valueOf("1995-01-01"),Date.valueOf("1999-01-01"))
+    @Test(dataProvider = "getData")
+    public void VisaTest(User u){
+        this.registrationPage.goTo();
+        this.registrationPage.setNames(u.getFirstName(),u.getLastName());
+        this.registrationPage.setCountryFromAndTo(u.getFromCountry(),u.getToCountry());
+        this.registrationPage.setBirthDate(u.getDob().toInstant()
+                                .atZone(ZoneId.systemDefault())
+                                .toLocalDate());
+
+        this.registrationPage.setContactDetails(u.getEmail(),u.getPhone());
+        this.registrationPage.setComments(u.getComments());
+        this.registrationPage.submit();
+        System.out.println(
+                this.registrationPage.getConfirmationNumber()
+        );
+    }
+
+    @DataProvider
+    public Object[] getData(){
+        return this.repository.findByDobBetween(Date.valueOf("1995-01-01"),Date.valueOf("1999-01-01"))
                 .stream()
                 .limit(3)
-                .collect(Collectors.toList());
-
-        for(User u : users){
-            this.registrationPage.goTo();
-            this.registrationPage.setNames(u.getFirstName(),u.getLastName());
-            this.registrationPage.setCountryFromAndTo(u.getFromCountry(),u.getToCountry());
-            this.registrationPage.setBirthDate(u.getDob().toInstant()
-                                    .atZone(ZoneId.systemDefault())
-                                    .toLocalDate());
-
-            this.registrationPage.setContactDetails(u.getEmail(),u.getPhone());
-            this.registrationPage.setComments(u.getComments());
-            this.registrationPage.submit();
-            System.out.println(
-                    this.registrationPage.getConfirmationNumber()
-            );
-        }
+                .toArray();
     }
 
 }
