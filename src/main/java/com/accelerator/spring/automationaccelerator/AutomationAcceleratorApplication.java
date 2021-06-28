a17ca9ba63e120b4ddeeb54ef65070e6c4f22ee2
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-28 08:25:12 +0530

    Solution to extend reports issue using spring

--- src/main/java/com/accelerator/spring/automationaccelerator/AutomationAcceleratorApplication.java
+++ src/main/java/com/accelerator/spring/automationaccelerator/AutomationAcceleratorApplication.java
@@ -2,8 +2,9 @@ package com.accelerator.spring.automationaccelerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class AutomationAcceleratorApplication {
 
 	public static void main(String[] args) {
